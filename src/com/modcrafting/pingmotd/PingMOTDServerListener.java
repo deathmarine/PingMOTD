e45f2caf80625a5702fa1c0cf98dbbbbccc3cc6e
deathmarine <josh@mcjoshmod.com>
2011-12-05 11:33:04 -0500

    Cleaned up code.

--- src/com/modcrafting/pingmotd/PingMOTDServerListener.java
+++ src/com/modcrafting/pingmotd/PingMOTDServerListener.java
@@ -12,7 +12,6 @@ public class PingMOTDServerListener extends ServerListener{
 	public void onServerListPing(ServerListPingEvent event){
 		String Msg = plugin.newMsg();
 		if (Msg == null){
-			Msg = event.getMotd();
 			event.setMotd(event.getMotd());			
 		}else{
 			event.setMotd(plugin.newMsg());
